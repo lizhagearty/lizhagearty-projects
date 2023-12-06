b6d5c1bd80a1a411b970d50b92cc3e8d58ad59b4
lizhagearty <lizhagearty@gmail.com>
2023-12-06 10:18:54 -0800

    changing visual simulator to circles with matter physics

--- visual-simulator/src/App.js
+++ visual-simulator/src/App.js
@@ -1,14 +1,16 @@
 // src/App.js
 import React from 'react';
 import './App.css';
-import RippleEffect from './RippleEffect';
+// import RippleEffect from './RippleEffect';
+import PhysicsCircle from './PhysicsCircle';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <RippleEffect />
+      {/* <RippleEffect /> */}
+      <PhysicsCircle />
     </div>
   );
 }
---
+++ visual-simulator/src/PhysicsCircle.js
@@ -0,0 +1,82 @@
+// src/PhysicsCircle.js
+import React, { useEffect, useRef } from 'react';
+import Matter from 'matter-js';
+import Sketch from 'react-p5';
+
+let engine;
+let world;
+let circles = [];
+let boundaries = [];
+
+const PhysicsCircle = () => {
+  const p5ContainerRef = useRef();
+
+  const setup = (p5, canvasParentRef) => {
+    p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(canvasParentRef);
+    
+    engine = Matter.Engine.create();
+    world = engine.world;
+    world.gravity.y = 1;
+
+    // Add boundaries (e.g., ground)
+    const ground = Matter.Bodies.rectangle(p5.width / 2, p5.height, p5.width, 20, { isStatic: true });
+    boundaries.push(ground);
+    Matter.World.add(world, ground);
+  };
+
+  const draw = (p5) => {
+    p5.background('#f844b0'); 
+    Matter.Engine.update(engine);
+
+    // Render circles
+    for (let circle of circles) {
+      p5.push();
+      p5.fill(255);
+      p5.circle(circle.position.x, circle.position.y, circle.circleRadius * 2);
+      p5.pop();
+    }
+
+    // Render boundaries
+    p5.push();
+    p5.fill(0);
+    for (let boundary of boundaries) {
+      p5.rect(boundary.position.x, boundary.position.y, p5.width, 20);
+    }
+    p5.pop();
+  };
+
+  const mousePressed = (p5) => {
+    // Create a circle body at the mouse position
+    let circle = Matter.Bodies.circle(p5.mouseX, p5.mouseY, 100, { restitution: 0.6 });
+    circles.push(circle);
+    Matter.World.add(world, circle);
+  };
+
+  const mouseReleased = (p5) => {
+    // When the mouse is released, apply a force to make the circle fall
+    for (let circle of circles) {
+      Matter.Body.setVelocity(circle, { x: 0, y: 5 });
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('deviceorientation', handleOrientation, true);
+
+    return () => {
+      window.removeEventListener('deviceorientation', handleOrientation);
+    }
+  }, []);
+
+  const handleOrientation = (event) => {
+    // Adjust gravity according to device orientation
+    let x = event.gamma; // In degree in the range [-90,90]
+    let y = event.beta; // In degree in the range [-180,180]
+
+    Matter.World.gravity.x = x * 0.01;
+    Matter.World.gravity.y = y * 0.01;
+  };
+
+  return <Sketch setup={setup} draw={draw} mousePressed={mousePressed} mouseReleased={mouseReleased} ref={p5ContainerRef} />;
+};
+
+export default PhysicsCircle;
